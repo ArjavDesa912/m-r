ff0c50b262f8d2d83e64d970ace64ab69fcf2cb0
Arjav Desai <akdesai912@gmail.com>
2020-04-04 23:11:35 +0530

    Updates

---
+++ precache-manifest.78c976425cfc1daaacd4b4bb8a9ab9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04088ed66eded703ebbd05d2b52a4214",
+    "url": "/ArjavDesa912/m-r/index.html"
+  },
+  {
+    "revision": "accacad7a0c05808648c",
+    "url": "/ArjavDesa912/m-r/static/css/main.309e2eed.chunk.css"
+  },
+  {
+    "revision": "a18d43196547948386f9",
+    "url": "/ArjavDesa912/m-r/static/js/2.8ebdf4f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ArjavDesa912/m-r/static/js/2.8ebdf4f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "accacad7a0c05808648c",
+    "url": "/ArjavDesa912/m-r/static/js/main.a91ba5fc.chunk.js"
+  },
+  {
+    "revision": "0661574d965b5536aee0",
+    "url": "/ArjavDesa912/m-r/static/js/runtime-main.bf50a5bd.js"
+  }
+]);
